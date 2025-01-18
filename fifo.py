7f81b05338efb7daccdb8f75e35d741bd4c83e59
Lovro Oreskovic <lovro@oreskovic.me>
2025-01-18 18:02:50 +0100

    move everything to one function, will portion it later

--- fifo.py
+++ fifo.py
@@ -25,48 +25,58 @@ from openpyxl import load_workbook
 class trading_export_sorter:
     def __init__(self, input_file):
         self.df = pd.read_csv(input_file)
-        self.prepare_data()
-        self.buy_and_sell = self.df.loc[self.df['Action'].isin(["Limit buy", "Limit sell", "Market buy", "Market sell"])].groupby('Ticker')
-
-    def prepare_data(self):
+        print(self.df.columns)
+        print(f"Actions in the file: {self.df['Action'].unique()}")
         # invert values of currency conversion fees
         self.df['Currency conversion fee'] *= -1
         # invert values of buy trades
         buy_columns_to_invert = ['Total', 'No. of shares']
         self.df.loc[self.df['Action'].str.lower().str.contains('buy'), buy_columns_to_invert] *= -1
-        
-        
-    def prepare_main_sheet(self):
-        def get_sum(column_name):
-            return {column_name:self.df.loc[self.df['Action'] == column_name]['Total'].sum()}
-        
-        data = {}
-        data.update(get_sum("Interest on cash"))
-        data.update(get_sum('Lending Interest'))
-        data.update(get_sum('Deposit'))
-        data.update(get_sum('Dividend'))
-        data.update(get_sum('Dividend Manufactured Payment'))
-        data.update(get_sum('New Card Cost'))
-        data.update({'Currency Conversion Fees': self.df["Currency conversion fee"].sum()})
+        self.buy_and_sell = self.df.loc[self.df['Action'].isin(["Limit buy", "Limit sell", "Market buy", "Market sell"])].groupby('Ticker')
 
-        df_main = pd.DataFrame(list(data.items()))
-        return df_main
-        
-    def prepare_ticker(self, ticker_name):
-        columns_to_sum = ['Total', 'No. of shares', 'Result', 'Currency conversion fee']
-        ticker = self.buy_and_sell.get_group(ticker_name)
-        # add a total row
-        sums = ticker[columns_to_sum].sum(numeric_only=True)
-        sums.name = 'Total'
-        return pd.concat([ticker, sums.to_frame().T])
-    
-    def export_all(self, output_file):
+    def do_work(self, output_file):
+        result_sum = 0
         with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
-            self.prepare_main_sheet().to_excel(writer, sheet_name='Main', index=False, header=False)
             for ticker_name in self.buy_and_sell.groups.keys():
+                columns_to_sum = ['Total', 'No. of shares', 'Result', 'Currency conversion fee']
+                ticker = self.buy_and_sell.get_group(ticker_name)
+                # add a total row
+                sums = ticker[columns_to_sum].sum(numeric_only=True)
+                sums.name = 'Total'
+                ticker = pd.concat([ticker, sums.to_frame().T])
+                result_sum += sums['Result']
                 print(f"exporting ticker: {ticker_name}")
-                self.prepare_ticker(ticker_name).to_excel(writer, sheet_name=ticker_name, index=True)
+                ticker.to_excel(writer, sheet_name=ticker_name, index=True)
+
+            def get_sum(column_name):
+                return {column_name:self.df.loc[self.df['Action'] == column_name]['Total'].sum()}
+            
+            data = dict()
+            data.update(get_sum('Interest on cash'))
+            data.update(get_sum('Lending interest'))
+            data.update(get_sum('Deposit'))
+            data.update(get_sum('Dividend (Dividend)'))
+            data.update(get_sum('Dividend (Dividend manufactured payment)'))
+            data.update(get_sum('New card cost'))
+            data.update({'Currency Conversion Fees': self.df["Currency conversion fee"].sum()})
+
+            data.update({'Total results': result_sum})
+
+            df_main = pd.DataFrame(list(data.items()))
+            df_main.to_excel(writer, sheet_name='Main', index=False, header=False)
+
         self.adjust_column_widths(output_file)
+        self.move_main_sheet_to_front(output_file)
+        
+    def move_main_sheet_to_front(self, output_file):
+        # Load the workbook to move the main sheet to the front
+        workbook = load_workbook(output_file)
+        sheets = workbook.sheetnames
+        main_sheet = workbook['Main']
+        workbook.remove(main_sheet)
+        workbook.create_sheet('Main', 0)
+        workbook.save(output_file)
+    
         
     def adjust_column_widths(self, output_file): 
         # Load the workbook to adjust column widths
@@ -80,13 +90,13 @@ class trading_export_sorter:
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Process trading data from CSV.")
+    parser = argparse.ArgumentParser(description="Process exported data from trading212.")
     parser.add_argument("--output-file", default="output.xlsx", help="Path to the output Excel file")
     parser.add_argument("csv_file", help="Path to the trading212 export CSV file")
     args = parser.parse_args()
 
     sorter = trading_export_sorter(args.csv_file)
-    sorter.export_all(args.output_file)
+    sorter.do_work(args.output_file)
 
 if __name__ == "__main__":
     main()
