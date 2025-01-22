c1cd1b884fc3ee779ec2103c91610d268b328414
Lovro Oreskovic <lovro@oreskovic.me>
2025-01-22 22:02:38 +0100

    add conversion rate for the whole year

--- fifo.py
+++ fifo.py
@@ -10,7 +10,6 @@ from dataclasses import dataclass
 # - do the fifo calculation
 # - keep track of conversion gains/losses
 
-
 # negative values decrement the cash on balance
 # so buy trades are negative
 # new card cost is negative
@@ -26,10 +25,12 @@ from dataclasses import dataclass
 
 
 class trading_export_sorter:
-    def __init__(self, input_file):
+    def __init__(self, input_file, debug=False):
+        self.debug = debug
+
         self.df = pd.read_csv(input_file)
-        #print(self.df.columns)
-        #print(f"Actions in the file: {self.df['Action'].unique()}")
+        self.debug_print(self.df.columns)
+        self.debug_print(f"Actions in the file: {self.df['Action'].unique()}")
         # invert values of currency conversion fees
         self.df['Currency conversion fee'] *= -1
         # invert values of buy trades
@@ -37,7 +38,7 @@ class trading_export_sorter:
         self.df.loc[self.df['Action'].str.lower().str.contains('buy'), buy_columns_to_invert] *= -1
         self.buy_and_sell = self.df.loc[self.df['Action'].isin(["Limit buy", "Limit sell", "Market buy", "Market sell"])].groupby('Ticker')
        
-    def calculate_fifo(self, ticker):
+    def calculate_fifo_for_ticker(self, ticker):
         Trade = namedtuple('Trade', ['shares', 'price'])
         @dataclass
         class Trade:
@@ -60,21 +61,28 @@ class trading_export_sorter:
                         result += (row['Price / share'] - trade.price) * trade.shares
                         trade.shares = 0
         resulting_shares = sum(trade.shares for trade in trades)
-        print(f"result: {row["Ticker"]} {resulting_shares} {result}")
+        self.debug_print(f"result: {row["Ticker"]} {resulting_shares} {result}")
+        return result
 
     def do_work(self, output_file):
         result_sum = 0
+        fifo_result_sum = 0
         with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
             for ticker_name in self.buy_and_sell.groups.keys():
                 ticker = self.buy_and_sell.get_group(ticker_name)
-                self.calculate_fifo(ticker)
+                result = self.calculate_fifo_for_ticker(ticker)
+                fifo_result_sum += result
                 # add a total row
                 columns_to_sum = ['Total', 'No. of shares', 'Result', 'Currency conversion fee']
                 sums = ticker[columns_to_sum].sum(numeric_only=True)
                 sums.name = 'Total'
                 ticker = pd.concat([ticker, sums.to_frame().T])
                 result_sum += sums['Result']
-                print(f"exporting ticker: {ticker_name}")
+                # add a fifo row
+                fifo_row = pd.DataFrame({'Action': 'Fifo', 'Result': result}, index=[0])
+                ticker = pd.concat([ticker, fifo_row])
+                
+                self.debug_print(f"exporting ticker: {ticker_name}")
                 ticker.to_excel(writer, sheet_name=ticker_name, index=True)
 
             def get_sum(column_name):
@@ -89,6 +97,8 @@ class trading_export_sorter:
             data.update(get_sum('New card cost'))
             data.update({'Currency Conversion Fees': self.df["Currency conversion fee"].sum()})
             data.update({'Total results': result_sum})
+            data.update({'Fifo total results': fifo_result_sum})
+            self.debug_print(data)
 
             df_main = pd.DataFrame(list(data.items()))
             df_main.to_excel(writer, sheet_name='Main', index=False, header=False)
@@ -97,12 +107,11 @@ class trading_export_sorter:
         self.move_main_sheet_to_front(output_file)
         
     def move_main_sheet_to_front(self, output_file):
-        # Load the workbook to move the main sheet to the front
         workbook = load_workbook(output_file)
-        sheets = workbook.sheetnames
-        main_sheet = workbook['Main']
-        workbook.remove(main_sheet)
-        workbook.create_sheet('Main', 0)
+        sheet_names = workbook.sheetnames
+        if 'Main' in sheet_names and sheet_names[0] != 'Main':
+            main_idx = sheet_names.index('Main')
+            workbook._sheets.insert(0, workbook._sheets.pop(main_idx))
         workbook.save(output_file)
     
         
@@ -115,15 +124,21 @@ class trading_export_sorter:
                 length = max(len(str(cell.value)) for cell in column_cells)
                 worksheet.column_dimensions[column_cells[0].column_letter].width = length + 3
         workbook.save(output_file)
+        
+    def debug_print(self, *args, **kwargs):
+        if self.debug:
+            print(*args, **kwargs)
+
 
 
 def main():
     parser = argparse.ArgumentParser(description="Process exported data from trading212.")
     parser.add_argument("--output-file", default="output.xlsx", help="Path to the output Excel file")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode")
     parser.add_argument("csv_file", help="Path to the trading212 export CSV file")
     args = parser.parse_args()
 
-    sorter = trading_export_sorter(args.csv_file)
+    sorter = trading_export_sorter(args.csv_file, debug=args.debug)
     sorter.do_work(args.output_file)
 
 if __name__ == "__main__":
